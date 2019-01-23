0f9ac9c4b134ff4051463ae524589d01c5d7b2bc
SICPRobotics <ignatiusrobotics@gmail.com>
2019-01-23 16:00:49 -0600

    first commit

---
+++ 2019 Robot Code/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,67 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveTrain;
+
+public class Robot extends TimedRobot 
+{
+  public static DriveTrain driveTrain = new DriveTrain();
+  public static OI m_oi;
+
+  Command m_autonomousCommand;
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+  @Override
+  public void robotInit() {
+    m_oi = new OI();
+    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
+    // chooser.addOption("My Auto", new MyAutoCommand());
+    SmartDashboard.putData("Auto mode", m_chooser);
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_chooser.getSelected();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.start();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() 
+  {
+    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void teleopInit() 
+  {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() 
+  {
+    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void testPeriodic() {}
+}
---
+++ 2019 Robot Code/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveTrain extends Subsystem 
+{
+  public DriveTrain()
+  {
+
+  }
+  
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
