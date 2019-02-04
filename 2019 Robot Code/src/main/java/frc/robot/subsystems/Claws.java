7019c16e26e66ac019a9a840d3d80d9986b07882
unknown <ignatiusrobotics@gmail.com>
2019-02-04 17:23:49 -0600

    added claws subsystem

---
+++ 2019 Robot Code/src/main/java/frc/robot/subsystems/Claws.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Claws extends Subsystem 
+{
+  public Claws()
+  {
+    
+  }
+  
+  @Override
+  public void initDefaultCommand() 
+  {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
